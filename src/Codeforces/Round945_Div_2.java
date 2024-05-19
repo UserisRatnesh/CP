073f677a1beb55ca0f2d830f03356a6c89528365
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2024-05-19 18:58:41 +0530

    Round945_Div_2, A solution

--- src/Codeforces/Round945_Div_2.java
+++ src/Codeforces/Round945_Div_2.java
@@ -19,29 +19,23 @@ public class Round945_Div_2
 			int p2 = sc.nextInt();
 			int p3 = sc.nextInt();
 			
-			aHelper(p1,p2,p3);
+			if ((p1 + p2 + p3) % 2 != 0) {
+	            // If the total points are odd then score not possible
+	            System.out.println(-1);
+	        } else {
+	            int sum = p1 + p2 ;
+	            int maxDraws = Math.min(sum, p3);
+	            if(sum-p3 > 0)
+	            {
+	            	maxDraws += Math.abs(sum-p3)/2;
+	            }
+	            System.out.println(maxDraws);
+	        }
 			
 			
 		}
 		sc.close();
 	}
-	
-	private static void aHelper(int p1, int p2, int p3)
-	{
-		if ((p1 + p2 + p3) % 2 != 0) {
-            // If the total points are odd or the highest score is more than the sum of the other two scores, it's impossible
-            System.out.println(-1);
-        } else {
-            int sum = p1 + p2 ;
-            int maxDraws = Math.min(sum, p3);
-            if(sum-p3 > 0)
-            {
-            	maxDraws += Math.abs(sum-p3)/2;
-            }
-            System.out.println(maxDraws);
-        }
-	}
-	
 	
 	
 	private static void b()
