137f560958674609fd753ff39ae7b1253c2096a9
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2024-06-30 19:09:22 +0530

    Could solve only one question, Worst contest ever.

---
+++ src/Codeforces/Educational_Round_167_Div_2.java
@@ -0,0 +1,44 @@
+package Codeforces;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.Arrays;
+
+
+public class Educational_Round_167_Div_2 
+{
+
+	private static void a()
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i=0; i<n; i++)
+		{
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			if(y < -1)
+			{
+				System.out.println("NO");
+			}
+			else
+			{
+				System.out.println("YES");
+			}
+		}
+		sc.close();
+	}
+
+	
+
+
+	public static void main(String[] args) 
+	{
+		
+	}
+
+}
